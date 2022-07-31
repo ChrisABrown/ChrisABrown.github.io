dbba1266cd75d60ccd0ef284c8358d8c76917ef0
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-07-31 12:37:17 -0500

    filled out movieslist component

--- movie_reviews/frontend/src/components/MoviesList.js
+++ movie_reviews/frontend/src/components/MoviesList.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import MovieDataService from "../service/movies";
+import { Link } from "react-router-dom";
 
-export default function MoviesList() {
-  return <div className="App">Movies List</div>;
-}
+const MoviesList = (props) => {
+  const [movies, setMovies] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+  const [searchRating, setSearchRating] = useState("");
+  const [ratings, setRatings] = useState(["All Ratings"]);
+};
