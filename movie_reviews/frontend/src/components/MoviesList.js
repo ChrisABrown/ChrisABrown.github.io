264b3d8738976be78bcbc06924f509484a61d4f8
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-08-05 12:59:29 -0500

    implemented pagination for movies list retrieval

--- movie_reviews/frontend/src/components/MoviesList.js
+++ movie_reviews/frontend/src/components/MoviesList.js
@@ -9,16 +9,25 @@ export const MoviesList = (props) => {
   const [searchTitle, setSearchTitle] = useState("");
   const [searchRating, setSearchRating] = useState("");
   const [ratings, setRatings] = useState(["All Ratings"]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [entriesPerPage, setEntriesPerPage] = useState(0);
 
   useEffect(() => {
     retrieveMovies();
     retrieveRatings();
   }, []);
+
+  useEffect(() => {
+    retrieveMovies();
+  }, [currentPage]);
+
   const retrieveMovies = () => {
-    MovieDataService.getAll()
+    MovieDataService.getAll(currentPage)
       .then((response) => {
         console.log(response.data);
         setMovies(response.data.movies);
+        setCurrentPage(response.data.page);
+        setEntriesPerPage(response.data.entries_per_page);
       })
       .catch((e) => {
         console.log(e);
@@ -116,6 +125,16 @@ export const MoviesList = (props) => {
             );
           })}
         </Row>
+        <br />
+        Showing page: {currentPage}
+        <Button
+          variant="link"
+          onClick={() => {
+            setCurrentPage(currentPage + 1);
+          }}
+        >
+          Get next {entriesPerPage} results
+        </Button>
       </Container>
     </>
   );
