cc5d3103284303d71a0833e036edb7cf6849f1c5
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-08-08 20:38:30 -0500

    app finished

--- movie_reviews/frontend/src/components/MoviesList.js
+++ movie_reviews/frontend/src/components/MoviesList.js
@@ -11,6 +11,7 @@ export const MoviesList = (props) => {
   const [ratings, setRatings] = useState(["All Ratings"]);
   const [currentPage, setCurrentPage] = useState(0);
   const [entriesPerPage, setEntriesPerPage] = useState(0);
+  const [currentSearchMode, setCurrentSearchMode] = useState("");
 
   useEffect(() => {
     retrieveMovies();
@@ -18,10 +19,25 @@ export const MoviesList = (props) => {
   }, []);
 
   useEffect(() => {
-    retrieveMovies();
+    setCurrentPage(0);
+  }, [currentSearchMode]);
+
+  useEffect(() => {
+    retrieveNextPage();
   }, [currentPage]);
 
+  const retrieveNextPage = () => {
+    if (currentSearchMode === "findByTitle") {
+      findByTitle();
+    } else if (currentSearchMode === "findByRating") {
+      findByRating();
+    } else {
+      retrieveMovies();
+    }
+  };
+
   const retrieveMovies = () => {
+    setCurrentSearchMode("");
     MovieDataService.getAll(currentPage)
       .then((response) => {
         console.log(response.data);
@@ -44,7 +60,7 @@ export const MoviesList = (props) => {
       });
   };
   const find = (query, by) => {
-    MovieDataService.find(query, by)
+    MovieDataService.find(query, by, currentPage)
       .then((response) => {
         console.log(response.data);
         setMovies(response.data.movies);
@@ -54,9 +70,11 @@ export const MoviesList = (props) => {
       });
   };
   const findByTitle = () => {
+    setCurrentSearchMode("findByTitle");
     find(searchTitle, "title");
   };
   const findByRating = () => {
+    setCurrentSearchMode("findByRating");
     if (searchRating === "All Ratings") {
       retrieveMovies();
     } else {
