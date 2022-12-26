484e086d34eb21153735281fc01ef94110ad64ce
ChrisABrown <punkystylin@yahoo.com>
2022-12-26 15:22:16 -0600

    added button to 'HomePage' to enter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import ProjectRoutes from "./Components/ProjectRoutes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ProjectRoutes />
+    </div>
+  );
 }
 
 export default App;
--- src/Views/HomePage/Home.jsx
+++ src/Views/HomePage/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div id="home-page">
+      <h1>Welcome to My Page</h1>
+      <button id="home-page-enter">Enter</button>
+    </div>
+  );
 };
