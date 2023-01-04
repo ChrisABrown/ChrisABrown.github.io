0901813fdd90c8bf817c8551a642907e87a481ab
ChrisABrown <punkystylin@yahoo.com>
2023-01-04 08:43:29 -0600

    added test cases

--- source/reactCode/cypressautomation/cypress/integration/examples/test1.js
+++ source/reactCode/cypressautomation/cypress/integration/examples/test1.js
@@ -1,6 +1,10 @@
 //cypress - Spec
+/// <reference types="Cypress" />
 describe("My First Test Case", function () {
   it("first test", function () {
     cy.visit("www.google.com");
+    cy.get(".gLFyf").type("bubble");
+    cy.wait(2000);
+    cy.get(".erkvQe").should("have.length.at.least", 1);
   });
 });
