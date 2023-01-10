e024452a1a98bac4158941a6fc2fcf357d9453af
ChrisABrown <punkystylin@yahoo.com>
2023-01-10 12:00:29 -0600

    updated demo test for iframe testing

--- source/reactCode/cypressautomation/cypress/integration/examples/demo.js
+++ source/reactCode/cypressautomation/cypress/integration/examples/demo.js
@@ -1,3 +1,17 @@
+///<reference types ="Cypress"/>
+///<reference types ="Cypress-iframe"/>
+import "cypress-iframe";
+
 describe("Demo", () => {
-  it("Frames Test", () => {});
+  it("Frames Test", () => {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    cy.frameLoaded("#courses-iframe");
+
+    cy.iframe().find("a[href*='mentorship']").eq(0).click();
+
+    //Doesn't find properly
+    cy.iframe().find("h1.pricing-title text-white ls-1").as("pricing-title");
+
+    cy.iframe().get("@pricing-title").should("have.length", 2);
+  });
 });
