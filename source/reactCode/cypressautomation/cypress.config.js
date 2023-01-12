393c005525691a7e1aa7a59efbae6f65068dbe64
ChrisABrown <punkystylin@yahoo.com>
2023-01-12 11:59:13 -0600

    cypress cloud integration

--- source/reactCode/cypressautomation/cypress.config.js
+++ source/reactCode/cypressautomation/cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   env: {
     url: "https://rahulshettyacademy.com",
   },
+  projectId: "qmvq2c",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
