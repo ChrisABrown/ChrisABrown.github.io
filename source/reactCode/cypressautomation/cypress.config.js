0291e642d16a0ef190f7e10efc1b53debcdbf008
ChrisABrown <punkystylin@yahoo.com>
2023-01-15 14:32:28 -0600

    Revert "dependecies added for cypress/browserify-preprocessor"
    This reverts commit c19476debd84e7547f5760f8eb299660b3badb6c.

--- source/reactCode/cypressautomation/cypress.config.js
+++ source/reactCode/cypressautomation/cypress.config.js
@@ -1,11 +1,5 @@
 const { defineConfig } = require("cypress");
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
-async function setupNodeEvents(on, config) {
-  await preprocessor.addCucumberPreprocessorPlugin(on, config);
-  on("file:preprocessor", browserify.default(config));
-}
 module.exports = defineConfig({
   defaultCommandTimeout: 6000,
   env: {
@@ -16,7 +10,12 @@ module.exports = defineConfig({
   },
   projectId: "jm4rui",
   e2e: {
-    setupNodeEvents,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      await.preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+      on("file:preprocessor", browserify.default(config));
+    },
     specPattern: "cypress/integration/examples/*.js",
   },
 });
