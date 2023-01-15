59a3db6cb89aa8a034db4dcafcac5dcddaf71488
ChrisABrown <punkystylin@yahoo.com>
2023-01-15 13:19:59 -0600

    added mochawesome and cucumber dependencies to package.json

--- source/reactCode/cypressautomation/cypress.config.js
+++ source/reactCode/cypressautomation/cypress.config.js
@@ -5,10 +5,16 @@ module.exports = defineConfig({
   env: {
     url: "https://rahulshettyacademy.com",
   },
+  retries: {
+    runMode: 1,
+  },
   projectId: "jm4rui",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      await.preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+      on("file:preprocessor", browserify.default(config));
     },
     specPattern: "cypress/integration/examples/*.js",
   },
