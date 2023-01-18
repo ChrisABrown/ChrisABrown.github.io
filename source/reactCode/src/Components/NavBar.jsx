548fad5b973673184e2158c85763bb65350fdf88
ChrisABrown <punkystylin@yahoo.com>
2023-01-17 20:25:29 -0600

    finished making navbar

--- source/reactCode/src/Components/NavBar.jsx
+++ source/reactCode/src/Components/NavBar.jsx
@@ -2,7 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function NavBar(props) {
-  return <div></div>;
+  return (
+    <div id="Navbar">
+      <ul name="navbar" id="navbar">
+        <li>
+          <a name="home-page" href="/">
+            Home
+          </a>
+        </li>
+        <li>
+          <a name="login-page" href="/login">
+            Login
+          </a>
+        </li>
+        <li>
+          <a name="menu-page" href="/menu">
+            Menu
+          </a>
+        </li>
+        <li>
+          <a name="cart-page" href="/cart">
+            Cart
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 NavBar.propTypes = {};
