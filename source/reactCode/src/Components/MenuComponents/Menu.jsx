7310008a20d428b3bafc0092f6ca572fc8bc5adc
ChrisABrown <punkystylin@yahoo.com>
2023-01-28 01:40:35 -0600

    added menulist structure and inventory.json file to Assets folder

--- source/reactCode/src/Components/MenuComponents/Menu.jsx
+++ source/reactCode/src/Components/MenuComponents/Menu.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import MenuList from "./MenuList";
+import inventory from "../../Assets/Inventory.json";
 
-export default function Menu() {
-  return <div>Menu</div>;
+export default function Menu({ inventory }) {
+  function addToCart() {}
+  function increment() {}
+  function decrement() {}
+
+  return (
+    <div id="menu-page">
+      <MenuList />
+    </div>
+  );
 }
