4585b5b53131b556fb251170d784d7cb964d878e
ChrisABrown <punkystylin@yahoo.com>
2023-02-06 11:22:55 -0600

    skeleton spring boot added to new branch for spring boot

---
+++ javaCode/TekTaco/src/main/java/com/personalproject/TekTaco/TekTacoApplication.java
@@ -0,0 +1,13 @@
+package com.personalproject.TekTaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekTacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekTacoApplication.class, args);
+	}
+
+}
