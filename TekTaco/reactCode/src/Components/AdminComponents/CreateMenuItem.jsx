3b247abe5a935b89c87d8775e87aec044c6027d3
ChrisABrown <punkystylin@yahoo.com>
2023-02-17 16:51:38 -0600

    useReducer added to createnewmenuitem, working on crud method to save newly made menu item

--- TekTaco/reactCode/src/Components/AdminComponents/CreateMenuItem.jsx
+++ TekTaco/reactCode/src/Components/AdminComponents/CreateMenuItem.jsx
@@ -1,20 +1,44 @@
+/* eslint-disable default-case */
 import React from "react";
-import { useState } from "react";
+import { useReducer } from "react";
 import { useNavigate } from "react-router-dom";
 import MenuItemService from "./utils.js";
 
+let lineBreak = <br />;
+const newMenuItem = {
+  id: "",
+  name: "",
+  image: "",
+  SKU: "",
+  price: "",
+  inStock: "",
+  description: "",
+};
+
+const reducerInputChange = (state, action) => {
+  switch (action.type) {
+    case "HANDLE INPUT TEXT":
+      return {
+        ...state,
+        [action.field]: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
 const CreateMenuItem = () => {
   let navigate = useNavigate();
-  let lineBreak = <br></br>;
-  const [newMenuItem, setNewMenuItem] = useState({
-    id: "",
-    name: "",
-    image: "",
-    SKU: "",
-    price: "",
-    inStock: "",
-    description: "",
-  });
+  const [menuItem, dispatch] = useReducer(reducerInputChange, newMenuItem);
+
+  const handleInputChange = (e) => {
+    dispatch({
+      type: "HANDLE INPUT TEXT",
+      field: e.target.name,
+      payload: e.target.value,
+    });
+  };
+
   const saveNewMenuItem = (e) => {
     e.prevent.default();
     MenuItemService.createNewMenuItem(newMenuItem).then((response) => {
@@ -22,109 +46,108 @@ const CreateMenuItem = () => {
     });
   };
 
-  const inputsHandler = (e) => {
-    setNewMenuItem(() => ({
-      ...newMenuItem,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  function handleSubmit(e) {
+  function handleSubmit() {
     alert("Menu Item was created with id: " + newMenuItem.id);
-    e.prevent.default();
   }
   return (
-    <div id="item-card">
-      <form onSubmit={handleSubmit} action="">
-        <div className="form-group">
-          <input
-            placeholder="Name"
-            name="Name"
-            type="text"
-            className="form-control"
-            value={newMenuItem.name}
-            onChange={(e) => {
-              setNewMenuItem(e.target.value);
-              console.log(e.target.value);
-            }}
-            required
-          ></input>
-          {lineBreak}
-          <input
-            placeholder="Id"
-            name="Id"
-            type="number"
-            className="form-control"
-            value={newMenuItem.id}
-            onChange={inputsHandler}
-            required
-          ></input>
-          {lineBreak}
-          <input
-            placeholder="Image Url"
-            name="Image Url"
-            type="url"
-            className="form-control"
-            value={newMenuItem.image}
-            onChange={inputsHandler}
-            required
-          ></input>
-          {lineBreak}
-          <input
-            placeholder="SKU"
-            name="SKU"
-            type="text"
-            className="form-control"
-            value={newMenuItem.SKU}
-            onChange={inputsHandler}
-            required
-          ></input>
-          {lineBreak}
-          <input
-            placeholder="Price"
-            name="Price"
-            type="number"
-            className="form-control"
-            value={newMenuItem.price}
-            onChange={inputsHandler}
-            required
-          ></input>
-          {lineBreak}
-          <input
-            placeholder="InStock"
-            name="InStock"
-            type="number"
-            className="form-control"
-            value={newMenuItem.inStock}
-            onChange={inputsHandler}
-            required
-          ></input>
-          {lineBreak}
-          <textarea
-            placeholder="Description"
-            name="Description"
-            className="form-control"
-            value={newMenuItem.description}
-            onChange={inputsHandler}
-            required
-          ></textarea>
-          {lineBreak}
+    <div id="cards-container2">
+      <h1>Create New Menu Item</h1>
+      <div id="item-card2">
+        <form action="">
+          <div className="form-group">
+            <label>Name:</label>
+            {lineBreak}
+            <input
+              name="name"
+              type="text"
+              className="form-control"
+              value={menuItem.name}
+              onChange={(e) => handleInputChange(e)}
+              required
+            ></input>
+            {lineBreak}
+            <label>Id:</label>
+            {lineBreak}
+            <input
+              name="id"
+              type="number"
+              className="form-control"
+              value={menuItem.id}
+              onChange={(e) => handleInputChange(e)}
+              required
+            ></input>
+            {lineBreak}
+            <label>Image URL:</label>
+            {lineBreak}
+            <input
+              name="image"
+              type="url"
+              className="form-control"
+              value={menuItem.image}
+              onChange={(e) => handleInputChange(e)}
+              required
+            ></input>
+            {lineBreak}
+            <label>SKU:</label>
+            {lineBreak}
+            <input
+              name="SKU"
+              type="text"
+              className="form-control"
+              value={menuItem.SKU}
+              onChange={(e) => handleInputChange(e)}
+              required
+            ></input>
+            {lineBreak}
+            <label>Price:</label>
+            {lineBreak}
+            <input
+              name="price"
+              type="number"
+              className="form-control"
+              value={menuItem.price}
+              onChange={(e) => handleInputChange(e)}
+              required
+            ></input>
+            {lineBreak}
+            <label># in Stock:</label>
+            {lineBreak}
+            <input
+              name="inStock"
+              type="number"
+              className="form-control"
+              value={menuItem.inStock}
+              onChange={(e) => handleInputChange(e)}
+              required
+            ></input>
+            {lineBreak}
+            <label>Description:</label>
+            {lineBreak}
+            <textarea
+              name="description"
+              className="form-control"
+              value={menuItem.description}
+              onChange={(e) => handleInputChange(e)}
+              required
+            ></textarea>
+            {lineBreak}
 
-          <div id="btn-console">
-            <button
-              id="btn-save"
-              type="submit"
-              onClick={saveNewMenuItem}
-              onClickCapture={() => navigate("/inventory")}
-            >
-              Save
-            </button>
-            <button id="btn-delete" onClick={() => navigate("/inventory")}>
-              Cancel
-            </button>
+            <div id="btn-console">
+              <button
+                id="btn-save"
+                type="submit"
+                onClick={saveNewMenuItem}
+                onClickCapture={() => navigate("/inventory")}
+              >
+                Save
+              </button>
+              <button id="btn-delete" onClick={() => navigate("/inventory")}>
+                Cancel
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
