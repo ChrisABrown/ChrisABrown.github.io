4a170d7fec657913ef6b0e15ad1885138e48a8b8
ChrisABrown <punkystylin@yahoo.com>
2023-02-28 07:28:02 -0600

    started over from scratch

---
+++ source/reactCode/src/Backend/DAO/menuItemDAO.js
@@ -0,0 +1,15 @@
+import mongodb from "mongodb";
+const ObjectId = mongodb.ObjectId;
+
+let menuItems;
+
+export default class MenuItemDAO {
+  static async injectDB(conn) {
+    if (menuItems) {
+      return;
+    }
+    try {
+      menuItems = await conn.db(process.env);
+    } catch (error) {}
+  }
+}
---
+++ source/reactCode/src/Server.js
@@ -0,0 +1,5 @@
+import pkg from "experess";
+import cors from "cors";
+import menuItems from "./api/menuItems.route.js";
+
+const { Express } = pkg;
